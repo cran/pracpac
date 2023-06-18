d2caf3d26363b0348580fbfc07b453bc9e44befe
VP Nagraj <nagraj@nagraj.net>
2023-06-18 21:40:05 +0000

    version 0.2.0

--- inst/doc/use-cases.R
+++ inst/doc/use-cases.R
@@ -13,10 +13,8 @@ options(rmarkdown.html_vignette.check_title = FALSE)
 #  
 #  ## specify the temp directory
 #  tmp <- tempdir()
-#  ## create a subdirectory of temp called "example"
-#  dir_create(path = path(tmp, "example"))
 #  ## copy the example hellow package to the temp directory
-#  dir_copy(path = system.file("hellow", package = "pracpac"), new_path = path(tmp, "example"))
+#  dir_copy(path = system.file("hellow", package = "pracpac"), new_path = path(tmp, "example", "hellow"))
 
 ## ---- eval=FALSE--------------------------------------------------------------
 #  dir_tree(path(tmp, "example", "hellow"), recurse = TRUE)
@@ -58,6 +56,93 @@ options(rmarkdown.html_vignette.check_title = FALSE)
 
 ## ---- eval = FALSE------------------------------------------------------------
 #  system("docker run --rm hellow:latest :es")
+
+## ---- echo=FALSE, eval=FALSE--------------------------------------------------
+#  ## cleanup needed in case on vignette rebuild the same tmp directory is picked
+#  dir_delete(path = path(tmp, "example"))
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  library(pracpac)
+#  library(fs)
+#  
+#  ## specify the temp directory
+#  tmp <- tempdir()
+#  ## copy the example ocf package to the temp directory
+#  dir_copy(path = system.file("ocf", package = "pracpac"), new_path = path(tmp, "example", "ocf"))
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  dir_tree(path(tmp, "example", "ocf"), recurse = TRUE)
+
+## ---- results='hide', eval=FALSE----------------------------------------------
+#  use_docker(pkg_path = path(tmp, "example", "ocf"), use_case = "shiny")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  dir_tree(path(tmp, "example", "ocf"), recurse = TRUE)
+
+## ---- eval=FALSE, echo=FALSE--------------------------------------------------
+#  # actually make those changes! don't echo and don't eval. need to edit app.R
+#  file_copy(system.file("example/ocf/app.R", package = "pracpac"), path(tmp, "example/ocf/docker/assets/app.R"), overwrite=TRUE)
+
+## ----eval=FALSE, echo=TRUE, code=readLines(system.file("example", "ocf", "app.R", package = "pracpac"))----
+#  library(shiny)
+#  library(ocf)
+#  
+#  # Define UI for application that draws a histogram
+#  ui <- fluidPage(
+#  
+#    # Application title
+#    titlePanel("Old Faithful Geyser Data (In Color!)"),
+#  
+#    # Sidebar with a slider input for number of bins
+#    sidebarLayout(
+#      sidebarPanel(
+#        sliderInput("bins",
+#                    "Number of bins:",
+#                    min = 1,
+#                    max = 50,
+#                    value = 30),
+#        actionButton(inputId = "color", label = "Add Color")
+#      ),
+#  
+#      # Show a plot of the generated distribution
+#      mainPanel(
+#        plotOutput("distPlot")
+#      )
+#    )
+#  )
+#  
+#  # Define server logic required to draw a histogram
+#  server <- function(input, output) {
+#  
+#    color_palette <- eventReactive(input$color, {
+#      get_pal()
+#    })
+#  
+#    output$distPlot <- renderPlot({
+#      # generate bins based on input$bins from ui.R
+#      x    <- faithful[, 2]
+#      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#  
+#      # draw the histogram with the specified number of bins
+#      hist(x,
+#           breaks = bins,
+#           col = color_palette()[[1]][1],
+#           border = color_palette()[[1]][5],
+#           main = paste0("Histogram in ", names(color_palette())))
+#    })
+#  }
+#  
+#  # Run the application
+#  shinyApp(ui = ui, server = server)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  build_image(pkg_path = path(tmp, "example", "ocf"))
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  system("docker images")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  system("docker run --rm -it -d --user shiny -p 3838:3838 ocf:0.1.0")
 
 ## ---- echo=FALSE, eval=FALSE--------------------------------------------------
 #  ## cleanup needed in case on vignette rebuild the same tmp directory is picked
--- tests/testthat/test_files.R
+++ tests/testthat/test_files.R
@@ -2,24 +2,29 @@ library(fs)
 library(withr)
 tmp <- tempdir()
 
+## NOTE: as of v0.2.0 none of the tests use renv snapshot per CRAN req
+
 test_that("use_docker pipeline creates expected directories and files with defaults", {
+
+  skip_on_cran()
 
   ex_pkg <- system.file("hellow", package = "pracpac")
   dir_create(path = path(tmp, "test"), recurse = TRUE)
   dir_copy(ex_pkg, path(tmp, "test"))
   with_tempdir({
-    use_docker(pkg_path = path(tmp, "test", "hellow"))
+    use_docker(pkg_path = path(tmp, "test", "hellow"), use_renv = FALSE)
   },
   tmpdir = tmp)
 
   expect_true(dir_exists(path(tmp, "test", "hellow", "docker")))
   expect_true(file_exists(path(tmp, "test", "hellow", "docker", "Dockerfile")))
-  expect_true(file_exists(path(tmp, "test", "hellow", "docker", "renv.lock")))
   expect_true(file_exists(path(tmp, "test", "hellow", "docker", "hellow_0.1.0.tar.gz")))
 
 })
 
 test_that("build_image returns build command as expected", {
+
+  skip_on_cran()
 
   # Check build command
   buildcmd <- suppressMessages(build_image(pkg_path = path(tmp, "test", "hellow"), build=FALSE))
@@ -34,6 +39,8 @@ test_that("build_image returns build command as expected", {
 
 
 test_that("Option to ignore renv works", {
+
+  skip_on_cran()
 
   ex_pkg <- system.file("hellow", package = "pracpac")
   dir_create(path = path(tmp, "test"), recurse = TRUE)
@@ -51,11 +58,13 @@ test_that("Option to ignore renv works", {
 
 test_that("Alternative directory structure works", {
 
+  skip_on_cran()
+
   ex_pkg <- system.file("hellow", package = "pracpac")
   dir_create(path = path(tmp, "test"), recurse = TRUE)
   dir_copy(ex_pkg, path(tmp, "test"))
   with_tempdir({
-    ## NOTE: setting use_renv to FALSE to make this test run quicker ... have already checked that option above
+    ## NOTE: setting use_renv to FALSE to make this test run quicker
     use_docker(pkg_path = path(tmp, "test", "hellow"), img_path = path(tmp, "test"), use_renv = FALSE)
   },
   tmpdir = tmp)
@@ -67,6 +76,8 @@ test_that("Alternative directory structure works", {
 })
 
 test_that("build_image returns build command as expected with alternative directory", {
+
+  skip_on_cran()
 
   # Check build command
   buildcmd <- suppressMessages(build_image(pkg_path = path(tmp, "test", "hellow"), img_path = path(tmp, "test"), build=FALSE))
@@ -81,11 +92,13 @@ test_that("build_image returns build command as expected with alternative direct
 
 test_that("The base image override option works", {
 
+  skip_on_cran()
+
   ex_pkg <- system.file("hellow", package = "pracpac")
   dir_create(path = path(tmp, "test"), recurse = TRUE)
   dir_copy(ex_pkg, path(tmp, "test"))
   with_tempdir({
-    ## NOTE: setting use_renv to FALSE to make this test run quicker ... have already checked that option above
+    ## NOTE: setting use_renv to FALSE to make this test run quicker
     use_docker(pkg_path = path(tmp, "test", "hellow"), use_renv = FALSE, base_image = "debian:10")
   },
   tmpdir = tmp)
@@ -98,32 +111,15 @@ test_that("The base image override option works", {
   dir_delete(path(tmp, "test"))
 })
 
-test_that("The other packages option works", {
-
-  ex_pkg <- system.file("hellow", package = "pracpac")
-  dir_create(path = path(tmp, "test"), recurse = TRUE)
-  dir_copy(ex_pkg, path(tmp, "test"))
-  with_tempdir({
-    ## NOTE: have to set use_renv = TRUE for this one ...
-    use_docker(pkg_path = path(tmp, "test", "hellow"), use_renv = TRUE, other_packages = "withr")
-  },
-  tmpdir = tmp)
-
-  ## check the base image
-  renvlock <- readLines(path(tmp, "test", "hellow", "docker", "renv.lock"))
-  expect_true(any(sapply(renvlock, function(x) grepl("withr", x))))
-
-  ## clean up
-  dir_delete(path(tmp, "test"))
-})
-
 test_that("Use case templates work (shiny)", {
 
+  skip_on_cran()
+
   ex_pkg <- system.file("hellow", package = "pracpac")
   dir_create(path = path(tmp, "test"), recurse = TRUE)
   dir_copy(ex_pkg, path(tmp, "test"))
   with_tempdir({
-    ## NOTE: setting use_renv to FALSE to make this test run quicker ... have already checked that option above
+    ## NOTE: setting use_renv to FALSE to make this test run quicker
     use_docker(pkg_path = path(tmp, "test", "hellow"), use_renv = FALSE, use_case = "shiny")
   },
   tmpdir = tmp)
@@ -140,11 +136,13 @@ test_that("Use case templates work (shiny)", {
 
 test_that("Use case templates work (rstudio)", {
 
+  skip_on_cran()
+
   ex_pkg <- system.file("hellow", package = "pracpac")
   dir_create(path = path(tmp, "test"), recurse = TRUE)
   dir_copy(ex_pkg, path(tmp, "test"))
   with_tempdir({
-    ## NOTE: setting use_renv to FALSE to make this test run quicker ... have already checked that option above
+    ## NOTE: setting use_renv to FALSE to make this test run quicker
     use_docker(pkg_path = path(tmp, "test", "hellow"), use_renv = FALSE, use_case = "rstudio")
   },
   tmpdir = tmp)
@@ -159,11 +157,13 @@ test_that("Use case templates work (rstudio)", {
 
 test_that("Use case templates work (pipeline)", {
 
+  skip_on_cran()
+
   ex_pkg <- system.file("hellow", package = "pracpac")
   dir_create(path = path(tmp, "test"), recurse = TRUE)
   dir_copy(ex_pkg, path(tmp, "test"))
   with_tempdir({
-    ## NOTE: setting use_renv to FALSE to make this test run quicker ... have already checked that option above
+    ## NOTE: setting use_renv to FALSE to make this test run quicker
     use_docker(pkg_path = path(tmp, "test", "hellow"), use_renv = FALSE, use_case = "pipeline")
   },
   tmpdir = tmp)
